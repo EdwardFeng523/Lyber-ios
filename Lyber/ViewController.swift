3c5e8984b6d7bf7c068b999e38df10fe3b26391e
edwardfeng <jf44@rice.edu>
2018-06-11 21:45:35 -0500

    first commit

---
+++ Lyber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lyber
+//
+//  Created by Edward Feng on 6/11/18.
+//  Copyright © 2018 Edward Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
