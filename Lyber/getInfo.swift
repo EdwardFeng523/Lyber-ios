6dd8afc1e149a0d5cccd458341fdcc04b0bd5d00
edwardfeng <jf44@rice.edu>
2018-06-13 22:42:22 -0500

    framework for parsing json

---
+++ Lyber/getInfo.swift
@@ -0,0 +1,13 @@
+//
+//  getInfo.swift
+//  Lyber
+//
+//  Created by Edward Feng on 6/13/18.
+//  Copyright © 2018 Edward Feng. All rights reserved.
+//
+
+import Foundation
+
+func sendRequest(longitude: String, latitude: String) {
+    
+}
