d30a2191fc3ffbc09b971d3c83c94e92fc40ec9b
edwardfeng <jf44@rice.edu>
2018-06-17 23:29:49 -0500

    added progress indicator but has problem

--- Lyber/ViewController.swift
+++ Lyber/ViewController.swift
@@ -23,9 +23,10 @@ class ViewController: UIViewController
     var items: [LyberItem] = [] { didSet{
         print ("items were set")
         displayTable.reloadData()
-        } }
+    } }
     
     @IBOutlet weak var displayTable: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,10 +75,58 @@ class ViewController: UIViewController
             alert.addAction(action)
             present(alert, animated: true, completion: nil)
         } else {
-            self.items = sendRequest(depar_lat: String(fromCoord!.latitude), depar_lng: String(fromCoord!.longitude), dest_lat: String(toCoord!.latitude), dest_lng: String(toCoord!.longitude))
+            items = sendRequest(depar_lat: String(fromCoord!.latitude), depar_lng: String(fromCoord!.longitude), dest_lat: String(toCoord!.latitude), dest_lng: String(toCoord!.longitude))
         }
     }
     
+    
+    func sendRequest(depar_lat: String, depar_lng: String, dest_lat: String, dest_lng: String) -> [LyberItem] {
+        let jsonUrlStringUber = "https://lyber-server.herokuapp.com/api/uber?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
+        let jsonUrlStringLyft = "https://lyber-server.herokuapp.com/api/lyft?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
+        
+        var uberFinished: Bool = false
+        var lyftFinished: Bool = false
+        
+        guard let urlUber = URL(string: jsonUrlStringUber) else { return [] }
+        guard let urlLyft = URL(string: jsonUrlStringLyft) else { return [] }
+        var lst: [LyberItem] = []
+        URLSession.shared.dataTask(with: urlUber) { (data, response, err) in
+            guard let data = data else { return }
+            do {
+                let uberInfo = try JSONDecoder().decode(UberInfo.self, from: data)
+                for elementUber in uberInfo.prices {
+                    let new_item = LyberItem(type: elementUber.display_name, description: "unavailable", priceRange: elementUber.estimate, high: Double(elementUber.high_estimate), low: Double(elementUber.low_estimate), distance: elementUber.distance, duration: elementUber.duration, estimatedArrival: "unavailable")
+                    lst.append(new_item)
+                }
+                print ("lst appended")
+                print (uberInfo)
+                uberFinished = true
+            } catch let jsonErr {
+                print("Error serializing json uber:", jsonErr)
+            }
+            }.resume()
+        
+        URLSession.shared.dataTask(with: urlLyft) { (data, response, err) in
+            guard let lyftData = data else { return }
+            do {
+                let lyftInfo = try JSONDecoder().decode(LyftInfo.self, from: lyftData)
+                for elementLyft in lyftInfo.cost_estimates {
+                    let new_item1 = LyberItem(type: elementLyft.ride_type, description: "unavailable", priceRange: "unavailable", high: Double(elementLyft.estimated_cost_cents_max), low: Double(elementLyft.estimated_cost_cents_min), distance: elementLyft.estimated_distance_miles, duration: elementLyft.estimated_duration_seconds, estimatedArrival: "unavailable")
+                    lst.append(new_item1)
+                }
+                print ("lst appended")
+                print (lyftInfo)
+                lyftFinished = true
+            } catch let jsonErr {
+                print("Error serializing json lyft:", jsonErr)
+            }
+            }.resume()
+        
+
+        while (!uberFinished || !lyftFinished) {
+        }
+        return lst
+    }
 
     
 }
--- Lyber/getInfo.swift
+++ Lyber/getInfo.swift
@@ -53,51 +53,51 @@ struct LyberItem {
     let estimatedArrival: String
 }
 
-func sendRequest(depar_lat: String, depar_lng: String, dest_lat: String, dest_lng: String) -> [LyberItem] {
-    let jsonUrlStringUber = "https://lyber-server.herokuapp.com/api/uber?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
-    let jsonUrlStringLyft = "https://lyber-server.herokuapp.com/api/lyft?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
-    
-    var uberFinished: Bool = false
-    var lyftFinished: Bool = false
-    
-    guard let urlUber = URL(string: jsonUrlStringUber) else { return [] }
-    guard let urlLyft = URL(string: jsonUrlStringLyft) else { return [] }
-    var lst: [LyberItem] = []
-    URLSession.shared.dataTask(with: urlUber) { (data, response, err) in
-        guard let data = data else { return }
-        do {
-            let uberInfo = try JSONDecoder().decode(UberInfo.self, from: data)
-            for elementUber in uberInfo.prices {
-                let new_item = LyberItem(type: elementUber.display_name, description: "unavailable", priceRange: elementUber.estimate, high: Double(elementUber.high_estimate), low: Double(elementUber.low_estimate), distance: elementUber.distance, duration: elementUber.duration, estimatedArrival: "unavailable")
-                lst.append(new_item)
-            }
-            print ("lst appended")
-            print (uberInfo)
-            uberFinished = true
-        } catch let jsonErr {
-            print("Error serializing json uber:", jsonErr)
-        }
-    }.resume()
-    
-    URLSession.shared.dataTask(with: urlLyft) { (data, response, err) in
-        guard let lyftData = data else { return }
-        do {
-            let lyftInfo = try JSONDecoder().decode(LyftInfo.self, from: lyftData)
-            for elementLyft in lyftInfo.cost_estimates {
-                let new_item1 = LyberItem(type: elementLyft.ride_type, description: "unavailable", priceRange: "unavailable", high: Double(elementLyft.estimated_cost_cents_max), low: Double(elementLyft.estimated_cost_cents_min), distance: elementLyft.estimated_distance_miles, duration: elementLyft.estimated_duration_seconds, estimatedArrival: "unavailable")
-                lst.append(new_item1)
-            }
-            print ("lst appended")
-            print (lyftInfo)
-            lyftFinished = true
-        } catch let jsonErr {
-            print("Error serializing json lyft:", jsonErr)
-        }
-    }.resume()
-    
-    while (!uberFinished || !lyftFinished) {
-        
-    }
-    print ("lst was", lst)
-    return lst
-}
+//func sendRequest(depar_lat: String, depar_lng: String, dest_lat: String, dest_lng: String) -> [LyberItem] {
+//    let jsonUrlStringUber = "https://lyber-server.herokuapp.com/api/uber?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
+//    let jsonUrlStringLyft = "https://lyber-server.herokuapp.com/api/lyft?depar_lat=" + depar_lat + "&depar_lng=" + depar_lng + "&dest_lat=" + dest_lat + "&dest_lng=" + dest_lng
+//    
+//    var uberFinished: Bool = false
+//    var lyftFinished: Bool = false
+//    
+//    guard let urlUber = URL(string: jsonUrlStringUber) else { return [] }
+//    guard let urlLyft = URL(string: jsonUrlStringLyft) else { return [] }
+//    var lst: [LyberItem] = []
+//    URLSession.shared.dataTask(with: urlUber) { (data, response, err) in
+//        guard let data = data else { return }
+//        do {
+//            let uberInfo = try JSONDecoder().decode(UberInfo.self, from: data)
+//            for elementUber in uberInfo.prices {
+//                let new_item = LyberItem(type: elementUber.display_name, description: "unavailable", priceRange: elementUber.estimate, high: Double(elementUber.high_estimate), low: Double(elementUber.low_estimate), distance: elementUber.distance, duration: elementUber.duration, estimatedArrival: "unavailable")
+//                lst.append(new_item)
+//            }
+//            print ("lst appended")
+//            print (uberInfo)
+//            uberFinished = true
+//        } catch let jsonErr {
+//            print("Error serializing json uber:", jsonErr)
+//        }
+//    }.resume()
+//    
+//    URLSession.shared.dataTask(with: urlLyft) { (data, response, err) in
+//        guard let lyftData = data else { return }
+//        do {
+//            let lyftInfo = try JSONDecoder().decode(LyftInfo.self, from: lyftData)
+//            for elementLyft in lyftInfo.cost_estimates {
+//                let new_item1 = LyberItem(type: elementLyft.ride_type, description: "unavailable", priceRange: "unavailable", high: Double(elementLyft.estimated_cost_cents_max), low: Double(elementLyft.estimated_cost_cents_min), distance: elementLyft.estimated_distance_miles, duration: elementLyft.estimated_duration_seconds, estimatedArrival: "unavailable")
+//                lst.append(new_item1)
+//            }
+//            print ("lst appended")
+//            print (lyftInfo)
+//            lyftFinished = true
+//        } catch let jsonErr {
+//            print("Error serializing json lyft:", jsonErr)
+//        }
+//    }.resume()
+//    
+//    while (!uberFinished || !lyftFinished) {
+//        
+//    }
+//    
+//    return lst
+//}
